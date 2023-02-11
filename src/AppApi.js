b40d35ba14c3b5a9a03d598f7b7ae842917115fe
Ganit kumar <104363798+ganitkumar517@users.noreply.github.com>
2023-02-12 00:33:25 +0530

    Update AppApi.js

--- src/AppApi.js
+++ src/AppApi.js
@@ -1,29 +1,31 @@
-const express = require("express");
-const mongodb = require("mongodb");
-
+const express = require('express');
 const app = express();
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-const MongoClient = mongodb.MongoClient;
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("customers");
-  app.post("/api/store-customer", (req, res) => {
-    const { customerName, mobileNumber, fruits,totalCost } = req.body;
-    
-    collection.insertOne({ customerName, mobileNumber, fruits, totalCost }, (err, result) => {
-      res.send({ success: true });
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'fruitDB';
+
+const client = new mongodb.MongoClient(url);
+
+client.connect(function(err) {
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  const collection = db.collection('fruits');
+
+  app.post('/api/store-value', (req, res) => {
+    console.log(req.body);
+    collection.insertOne(req.body, function(err, result) {
+      console.log("Inserted Successfully");
+      res.send(result);
     });
   });
-  app.get("/api/customers", (req, res) => {
-    collection.find({}).toArray((err, customers) => {
-      res.send(customers);
-    });
-  });
-});
 
-app.listen(3000, () => {
-  console.log("API listening on port 3000");
+  app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
+  });
 });
